addf5925bf12c3c42d423b2ed6281691de06942f
kwaku <44327202+mr-nansi@users.noreply.github.com>
2020-03-05 11:52:26 +0000

    idle save

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
   SECRET_KEY = os.environ.get('SECRET_KEY') or 'this-is-a-real-mystery'
+  SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    'sqlite:///' + os.path.join(basedir, 'app.db')
+  SQLALCHEMY_TRACK_MODIFICATIONS = False
