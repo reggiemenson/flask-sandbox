6c48c1caf167e8e1bbd3033050abb70f8df83ec3
kwaku <44327202+reggiemenson@users.noreply.github.com>
2020-03-18 12:51:58 +0000

    register done

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,9 @@
-from app import app
+from app import app, db
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
 from app.models import User
 
 
@@ -29,6 +29,20 @@ def logout():
   logout_user()
   return redirect(url_for('index'))
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  if current_user.is_authenticated:
+    return redirect(url_for('index'))
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    user = User(username=form.username.data, email=form.email.data)
+    user.set_password(form.password.data)
+    db.session.add(user)
+    db.session.commit()
+    flash('Congratulations, you are now a registered user!')
+    return redirect(url_for('login'))
+  return render_template('register.html', title='Register', form=form)
+  
 @app.route('/index')
 @app.route('/')
 @login_required
@@ -44,3 +58,4 @@ def index():
       }
       ]
   return render_template('index.html', title='Home', posts=posts)
+
